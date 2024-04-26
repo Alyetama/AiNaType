91ffab06bec4eb27c23cd27c676d36d10f381ccb
Alyetama <56323389+Alyetama@users.noreply.github.com>
2024-04-26 12:25:08 -0400

    Update

--- export_annotations.sh
+++ export_annotations.sh
@@ -8,12 +8,12 @@ export $(cat .env | xargs)
 curl -X GET \
     "$LABEL_STUDIO_URL/api/projects/5/export?exportType=JSON_MIN" \
     -H "Authorization: Token $API_KEY" \
-    --output  "project-5-at-$current_date-$random_string.json"
+    --output "project-5-at-$current_date-$random_string.json"
 
 curl -X GET \
     "$LABEL_STUDIO_URL/api/projects/5/export?exportType=JSON" \
     -H "Authorization: Token $API_KEY" \
-    --output  "project-5-full-at-$current_date-$random_string.json"
+    --output "project-5-full-at-$current_date-$random_string.json"
 
 echo "project-5-at-$current_date-$random_string.json"
 echo "project-5-full-at-$current_date-$random_string.json"
